5ebff1e83944dd83fb0b70da66f3e2f471ebddf5
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-18 18:20:39 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,8 @@
 def using_while
   levitation_force = 6
-	while 
-	puts "Wingardium Leviosa"
-	levitation_force++
+	while levitation_force < 10
+	  puts "Wingardium Leviosa"
+	  levitation_force++
 end
 
 
